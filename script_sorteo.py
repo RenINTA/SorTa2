9011449e84477da1250d07d84d5458cbc5e28fde
Melina Solovey <melinasolovey@gmail.com>
2021-06-01 12:57:11 -0300

    [M] No repeat the host twice
    Change it by Ivan 11.05.2021

--- script_sorteo.py
+++ script_sorteo.py
@@ -1,21 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Editor de Spyder
-
-Este es un archivo temporal.
-"""
-
+import json
 import random
-participantes = ['Meli','Santi','Cris', 'Ivan', 'Chiqui','Caro']
+
+with open('sorteo.json','r') as file:
+    data = json.load(file)
+
+participants = data['Participants']
+last_host = data['Last_host']
 
 print('Participantes: ', end = '' )
+[print(x, end = ' ' ) for x in participants]
 
-[print(x, end = ' ' ) for x in participantes]
+while True:
+    random.shuffle(participants)
+    if participants[0]!=last_host:
+        break
+    
+data['Last_host'] = participants[0]
 
-
-random.shuffle(participantes)
+try:
+    data['Times_being_host'][participants[0]] += 1 
+except KeyError:
+    data['Times_being_host'][participants[0]] = 1
 
 print('\nSorteo:')
 
-for i,j in enumerate(participantes):    
+for i,j in enumerate(participants):    
     print( str(i+1) + "- " + j )
+
+with open('sorteo.json','w') as file:
+    json.dump(data,file,indent=4)
+	
+print('\nVersion:11.05.IvanP')
